daa22558dd52e94211ff5241260e23f2ad14f75d
Onkar Lapate <onkar.lapate040@gmail.com>
2024-04-03 23:16:28 +0530

    Created register user API and html page. Restructrued views.

--- crm_website/urls.py
+++ crm_website/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from . import views
+from .views import user_view, home_view
 
 urlpatterns = [
-    path('', view=views.home, name='home'),
+    path('', view=home_view.home, name='home'),
+    path('login/', view=user_view.login_user, name='login'),
+    path('logout/', view=user_view.logout_user, name='logout'),
+    path('register/', view=user_view.register_user, name='register'),
 ]
